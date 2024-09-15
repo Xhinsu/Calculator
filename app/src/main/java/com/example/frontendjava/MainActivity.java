9c8e26f90bcf666591e4ec44619c324301ca079f
X <sabinarai@x.broadband>
2024-09-15 14:41:52 +0100

    feat: addition function added

--- app/src/main/java/com/example/frontendjava/MainActivity.java
+++ app/src/main/java/com/example/frontendjava/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.frontendjava;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +13,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText firstNum
-    private EditText secondNum;
-    private Button button
-            private TextView;
 
+public class MainActivity extends AppCompatActivity {
+    private EditText firstNum;
+    private EditText secondNum;
+    private TextView result;
+    private Button buttonAdd;
+    private Button buttonReset;
+
+
+
+    public int getAddition(){
+        int num1 = Integer.parseInt(firstNum.getText().toString());
+        int num2 =  + Integer.parseInt(secondNum.getText().toString());
+        return num2 +num1;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -28,8 +40,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Log.i(this.getLocalClassName(),"I m mega cool");
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonReset = findViewById(R.id.buttonReset);
+        firstNum = findViewById(R.id.firstNum);
+        secondNum = findViewById(R.id.secondNum);
+        result = findViewById(R.id.result);
 
+
+
+
+
+
+        buttonAdd.setOnClickListener(view -> result.setText(String.format("%d" ,getAddition())));
+        buttonReset.setOnClickListener(view -> result.setText(String.format("")));
     }
-
 
 }
